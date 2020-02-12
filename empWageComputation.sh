ea8a917a51877c6c8e360b3e3a0921cf1016f148
kanoop640 <kanoop640@gmail.com>
2020-02-12 16:17:41 +0530

    Attendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,9 @@
-echo "Welcome to Employee Wage problem computaion on master branch "
+rnd=$(( RANDOM%2 ))
+if(( $rnd==1 ))
+then
+        echo "Yes, Employee is present "
+else
+        echo "No, Employee is not present "
+fi
+
+
